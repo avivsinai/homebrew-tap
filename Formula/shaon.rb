c7d2cb0a04c2899cc6c5d590b1bf569b65d938ae
avivsinai <avivsinai@users.noreply.github.com>
2026-05-04 21:43:36 +0300

    shaon v0.9.0 (#9)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/shaon.rb
+++ Formula/shaon.rb
@@ -4,28 +4,28 @@
 class Shaon < Formula
   desc "Automate Hilanet attendance, payslips, salary, and reports for your own account"
   homepage "https://github.com/avivsinai/shaon"
-  version "0.8.3"
+  version "0.9.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/avivsinai/shaon/releases/download/v0.8.3/shaon-x86_64-apple-darwin.tar.gz"
-      sha256 "40ee634c9bf155c602a55f87103051eba6dc2de407f7eb6ec236e66ee419b320"
+      url "https://github.com/avivsinai/shaon/releases/download/v0.9.0/shaon-x86_64-apple-darwin.tar.gz"
+      sha256 "45a9b70d679239580fe56f3a5012a6b60dd5b3325a75a45514138ba05b2957ee"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/avivsinai/shaon/releases/download/v0.8.3/shaon-aarch64-apple-darwin.tar.gz"
-      sha256 "9670e4081c34978da189b7e99ef725c4cb9b732823fe1375ea7f453d9dc25c68"
+      url "https://github.com/avivsinai/shaon/releases/download/v0.9.0/shaon-aarch64-apple-darwin.tar.gz"
+      sha256 "18bec59374fdc6704a53c508c91bcdcf6e437f632657ddfe34d830a6c0928bf2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/avivsinai/shaon/releases/download/v0.8.3/shaon-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "280f22b9d144c07978a53fb6341f3b633343f57e731e100590e3ec391c760ccc"
+      url "https://github.com/avivsinai/shaon/releases/download/v0.9.0/shaon-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3531948d96d162409c30dd203b34166eaf173deed5cbcd94a98aa2c4ebaefef8"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/avivsinai/shaon/releases/download/v0.8.3/shaon-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "dc142c98362947668d2e3b3f81b1b832a9205b540055ea420ff46312a5f59b95"
+      url "https://github.com/avivsinai/shaon/releases/download/v0.9.0/shaon-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "059d351720af800892c104a9419f81a4bed95837d7ba4b772f585c054fa61d30"
     end
   end
 
